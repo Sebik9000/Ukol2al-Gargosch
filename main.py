ed35851c087cd4aeebbc4164eed0e2a8679ab080
Sebik9000 <159123960+Sebik9000@users.noreply.github.com>
2024-11-20 10:15:23 +0100

    dobre no array 2 done bubble sort

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import random
 
 #Vytvoření pole s 10ti náhodnými hodnotami (0-100)
 array = [random.randint(0, 100) for _ in range(10)]
-print("Původní pole:", array)
+print("ÚKOL 1-Původní pole:", array)
 
 #Seřazení pole
 sorted_array = sorted(array)
-print("Seřazené pole:", sorted_array)
+print("ÚKOL 1-Seřazené pole:", sorted_array)
 
 #Budu pracovat na bubble sortu
+
+array2 = [random.randint(0, 100) for _ in range(10)]
+print("ÚKOL 2-Původní pole:", array2)
+
+n = len(array2)
+for i in range(n-1):
+    for j in range(n-i-1):
+        if array2[j] > array2[j+1]:
+            array2[j], array2[j+1] = array2[j+1], array2[j]
+
+print("ÚKOL 2-Bubble sorted pole:", array2)
