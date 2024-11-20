40d86fcd2f5fe6f11822c16db0a13da7a0cc4c69
Sebastian Gargosch <98969@student.vitkovickastredni.cz>
2024-11-20 08:38:50 +0100

    pole

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import random
 
+#Vytvoření pole s 10ti náhodnými hodnotami (0-100)
+array = [random.randint(0, 100) for _ in range(10)]
+print("Původní pole:", array)
+
+#Seřazení pole
+sorted_array = sorted(array)
+print("Seřazené pole:", sorted_array)
